ee716f7f21c852185b2fd721a03662b4bf2564dd
robin <robin.github@gmail.com>
2019-06-10 14:08:11 +0700

    add php-json module

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ TAG="php"
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-docker build -t indexing/$TAG .
+docker build  -t indexing/$TAG . --network=host
+#docker build --build-arg http_proxy=http://10.10.5.55:3128 -t indexing/$TAG .
 docker push indexing/$TAG
